febdfa7ed4b5e4deff1fd32900b3f7d8f94d2283
Евгений Дулевский <naitmer2003@mail.ru>
2020-11-29 17:25:08 +0300

    Закончил 1 задачу

--- Поиск слова в 1 файле/Поиск слова/Form1.cs
+++ Поиск слова в 1 файле/Поиск слова/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,33 @@ namespace Поиск_слова
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (var read = new StreamReader(textBox1.Text))
+            {
+                int k = 0;
+                string msg;
+                while ((msg = await read.ReadLineAsync()) != null)
+                {
+                    string[] text = msg.Split(' ');
+                    if (checkBox1.Checked == false)
+                        foreach (string sr in text)
+                        {
+                            if (sr.ToUpper() == textBox2.Text.ToUpper())
+                                k++;
+                        }
+                    else
+                        foreach (string sr in text)
+                        {
+                            if (sr == textBox2.Text)
+                                k++;
+                        }
+                }
+                label3.Visible = true;
+                label3.Text = $"Слово {textBox2.Text}\nв файле {textBox1.Text}\nвстречается {k} раз";
+            }
         }
     }
 }
