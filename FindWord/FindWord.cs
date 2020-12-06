11f0eab809936984f91303f2cd83aacd716404be
Jebati <www.den-safonov@mail.ru>
2020-12-06 19:42:46 +0300

    FindWordOnFile

---
+++ FindWord/FindWord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FindWord
+{
+    public class FindWordSolver
+    {
+        public async Task<FindWordResult> OnFile(string path, string find)
+        {
+            FindWordResult result = new FindWordResult { Path = path  };
+
+            string content = File.ReadAllText(result.Path);
+            result.Count = FindOnContent(content, find);
+
+            return result;
+        }
+
+        public async Task<List<FindWordResult>> OnDirectory(string path, string find)
+        {
+            List<FindWordResult> result = new List<FindWordResult>();
+
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                string content = File.ReadAllText(file);
+                int count = FindOnContent(content, find);
+                if(count > 0)
+                {
+                    result.Add(new FindWordResult { Path = file, Count = count });
+                }
+            }
+
+            return result;
+        }
+
+        private int FindOnContent(string content, string find)
+        {
+            return Regex.Matches(content, find).Count;
+        }
+    }
+    public class FindWordResult
+    {
+        public int Count { get; set; } = 0;
+        public string Path { get; set; } = null;
+    }
+}
