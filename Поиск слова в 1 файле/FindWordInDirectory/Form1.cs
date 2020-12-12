51fc1ce819ffe2a634523d89beeca496e053ce27
Евгений Дулевский <naitmer2003@mail.ru>
2020-12-12 08:47:46 +0300

    Отредактировал ввод данных
    Теперь на не правильный ввод программа реагирует сообщением

--- Поиск слова в 1 файле/FindWordInDirectory/Form1.cs
+++ Поиск слова в 1 файле/FindWordInDirectory/Form1.cs
@@ -21,20 +21,30 @@ namespace FindWordInDirectory
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = " ";
-            string path = textBox1.Text.Remove(0, 1);
-            path = path.Remove(path.Length - 1, 1);
-            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);//получаем пути к файлам в директории
-            bool check = checkBox1.Checked;
-            for(int i=0;i<files.Count();i++)
+            try
             {
-                if(await FindWordClass.FindWordAsync(check, files.ElementAt(i), textBox2.Text)!=0)
+                richTextBox1.Text = " ";
+                string path = textBox1.Text.Remove(0, 1);
+                path = path.Remove(path.Length - 1, 1);
+                var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);//получаем пути к файлам в директории
+                bool check = checkBox1.Checked;
+                int k = 0;
+                for (int i = 0; i < files.Length; i++)
                 {
-                    richTextBox1.Text += "Название файла: " + Path.GetFileName(files.ElementAt(i));
-                    richTextBox1.Text += "\nПуть к файлу: " + files.ElementAt(i);
-                    richTextBox1.Text += $"\nКоличество вхождений слова {textBox2.Text}: " + await FindWordClass.FindWordAsync(check, files.ElementAt(i), textBox2.Text);
-                    richTextBox1.Text += "====================================================\n";
+                    if (await FindWordClass.FindWordAsync(check, files[i], textBox2.Text) != 0)
+                    {
+                        richTextBox1.Text += "Название файла: " + Path.GetFileName(files.ElementAt(i));
+                        richTextBox1.Text += "\nПуть к файлу: " + files.ElementAt(i);
+                        richTextBox1.Text += $"\nКоличество вхождений слова {textBox2.Text}: " + await FindWordClass.FindWordAsync(check, files.ElementAt(i), textBox2.Text);
+                        richTextBox1.Text += "====================================================\n";
+                        k++;
+                    }
                 }
+                if (k == 0) richTextBox1.Text = "В файлах данной директории файла с таким словом не обнаружено";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите путь к дирректории");
             }
         }
     }
--- Поиск слова в 1 файле/Поиск слова/Form1.cs
+++ Поиск слова в 1 файле/Поиск слова/Form1.cs
@@ -13,12 +13,20 @@ namespace Поиск_слова
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            string path = textBox1.Text.Remove(0, 1);
-            path = path.Remove(path.Length - 1, 1);
-            bool check = checkBox1.Checked;
-            label3.Visible = true;
-            label3.Text = $"Слово {textBox2.Text}\nв файле {textBox1.Text}\nвстречается {await FindWordClass.FindWordAsync(check, path, textBox2.Text)} раз";
-
+            try
+            {
+                string path = textBox1.Text.Remove(0, 1);
+                path = path.Remove(path.Length - 1, 1);
+                bool check = checkBox1.Checked;
+                label3.Visible = true;
+                if (await FindWordClass.FindWordAsync(check, path, textBox2.Text) == 0)
+                    label3.Text = $"Слово {textBox2.Text}\nв файле {textBox1.Text}\nне встречается";
+                else label3.Text = $"Слово {textBox2.Text}\nв файле {textBox1.Text}\nвстречается {await FindWordClass.FindWordAsync(check, path, textBox2.Text)} раз";
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Введите путь к файлу");
+            }
         }
     }
 }
