aa116580f96d98d35d8b9b31a59adc4e738600b1
vadiman2002 <vadiman2002@mail.com>
2020-12-23 19:32:38 +0300

    Задание сделал Сапрыкин Вадим ПКС-218

---
+++ FindWord/link/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace FindWordLib
+{
+    public class FindWordClass
+    {
+        static public async Task<int> FindWordAsync(bool check, string path, string textbox)
+        {
+            return await Task.Run(() => FindWord(check, path, textbox));
+        }
+
+        static int FindWord(bool check, string path, string textbox)
+        {
+            using (var read = new StreamReader(path))
+            {
+                int k = 0;
+                string msg;
+                while ((msg = read.ReadLine()) != null)
+                {
+                    string[] text = msg.Split(' ');
+                    if (check == false)
+                        foreach (string sr in text)
+                        {
+                            if (sr.ToUpper() == textbox.ToUpper())
+                                k++;
+                        }
+                    else
+                        foreach (string sr in text)
+                        {
+                            if (sr == textbox)
+                                k++;
+                        }
+                }
+                return k;
+            }
+        }
+    }
+}
